2e3c6e756d657aaf1ec6ba9c49711fca3bd5fcba
woodiertexas <woodytexas8088@gmail.com>
2022-07-23 16:12:24 -0700

    partial implementation of Palm Planks and their variants

--- src/main/java/io/github/debuggyteam/wonders/block/WondersBlocks.java
+++ src/main/java/io/github/debuggyteam/wonders/block/WondersBlocks.java
@@ -3,11 +3,12 @@ package io.github.debuggyteam.wonders.block;
 import io.github.debuggyteam.wonders.util.WondersRegistry;
 import net.minecraft.block.*;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.math.Direction;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
 public class WondersBlocks {
 	public static final Block TEST_BLOCK = WondersRegistry.registerBlock("test_block", createBlock(Material.WOOD, 2.0f));
+
+	// Palm tree logs
 	public static final Block PALM_LOG = WondersRegistry.registerBlock("palm_log", new PillarBlock(
 			QuiltBlockSettings.copy(Blocks.OAK_LOG).sounds(BlockSoundGroup.WOOD).strength(2.0f)
 	));
@@ -22,6 +23,23 @@ public class WondersBlocks {
 
 	public static final Block STRIPPED_PALM_WOOD = WondersRegistry.registerBlock("stripped_palm_wood", new PillarBlock(
 			QuiltBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).sounds(BlockSoundGroup.WOOD).strength(2.0f)
+	));
+
+	// Palm tree planks
+	public static final Block PALM_PLANKS = WondersRegistry.registerBlock("palm_planks", new Block(
+			QuiltBlockSettings.copy(Blocks.OAK_PLANKS).sounds(BlockSoundGroup.WOOD).strength(2.0f)
+	));
+
+	public static final StairsBlock PALM_STAIRS = (StairsBlock) WondersRegistry.registerBlock("palm_stairs", new StairsBlock(
+			Blocks.OAK_PLANKS.getDefaultState(), QuiltBlockSettings.copy(Blocks.OAK_STAIRS).sounds(BlockSoundGroup.WOOD).strength(2.0f)
+	));
+
+	public static final FenceBlock PALM_FENCE = (FenceBlock) WondersRegistry.registerBlock("palm_fence", new FenceBlock(
+			QuiltBlockSettings.copy(Blocks.OAK_FENCE).sounds(BlockSoundGroup.WOOD).strength(2.0f)
+	));
+
+	public static final SlabBlock PALM_SLAB = (SlabBlock) WondersRegistry.registerBlock("palm_slab", new SlabBlock(
+			QuiltBlockSettings.copy(Blocks.OAK_SLAB).sounds(BlockSoundGroup.WOOD).strength(2.0f)
 	));
 
 	private static Block createBlock(Material material, Float strength){
--- src/main/java/io/github/debuggyteam/wonders/item/WondersItems.java
+++ src/main/java/io/github/debuggyteam/wonders/item/WondersItems.java
@@ -11,10 +11,18 @@ public class WondersItems {
 
 	// registering block items
 	public static Item TEST_BLOCK = WondersRegistry.registerBlockItem("test_block", WondersBlocks.TEST_BLOCK);
+
+	// Palm logs
 	public static Item PALM_LOG = WondersRegistry.registerBlockItem("palm_log", WondersBlocks.PALM_LOG);
 	public static Item PALM_WOOD = WondersRegistry.registerBlockItem("palm_wood", WondersBlocks.PALM_WOOD);
 	public static Item STRIPPED_PALM_LOG = WondersRegistry.registerBlockItem("stripped_palm_log", WondersBlocks.STRIPPED_PALM_LOG);
 	public static Item STRIPPED_PALM_WOOD = WondersRegistry.registerBlockItem("stripped_palm_wood", WondersBlocks.STRIPPED_PALM_WOOD);
+
+	// Palm planks
+	public static Item PALM_PLANKS = WondersRegistry.registerBlockItem("palm_planks", WondersBlocks.PALM_PLANKS);
+	public static Item PALM_STAIRS = WondersRegistry.registerBlockItem("palm_stairs", WondersBlocks.PALM_STAIRS);
+	public static Item PALM_FENCE = WondersRegistry.registerBlockItem("palm_fence", WondersBlocks.PALM_FENCE);
+	public static Item PALM_SLAB = WondersRegistry.registerBlockItem("palm_slab", WondersBlocks.PALM_SLAB);
 
 	public static void init(){
 	}
